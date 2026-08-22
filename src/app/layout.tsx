ad90a64ee84e3657ca2f88bb611d11656495d845
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-22 11:01:03 +0200

    feat: wire cart provider into app shell

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { CartProvider } from "@/components/cart/CartProvider";
 import "./globals.css";
 import "./store.css";
 import "./catalogue.css";
+import "./checkout.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en-ZA"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full">{children}</body>
+      <body className="min-h-full">
+        <CartProvider>{children}</CartProvider>
+      </body>
     </html>
   );
 }
